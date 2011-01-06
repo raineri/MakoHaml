05ed8dbb3de024694f7ca7c8d752b6ebef9e0cf3
raineri <the.ech0@gmail.com>
2011-01-06 13:50:14 -0800

    renamed variables

--- makohaml/examples/example.py
+++ makohaml/examples/example.py
@@ -1,10 +1,10 @@
-from hamlpy import hamlpy
+from makohaml import makohaml
 
 f = open("example.hamlpy")
 haml = f.read()
 f.close()
 print haml
-parser = hamlpy.Compiler()
+parser = makohaml.Compiler()
 result = parser.process(haml)
 print "="*30
 print result
